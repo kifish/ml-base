cb9794daa48bf8cd8a57243011f808a25f921246
kifish <huakaihuakai@qq.com>
2019-03-04 16:04:35 +0800

    init

--- pku-deep-learning/lg-course/assignment1/code/main.py
+++ pku-deep-learning/lg-course/assignment1/code/main.py
@@ -1,14 +1,36 @@
 from utils import load_CIFAR10
-
+import numpy as np
 # Load the raw CIFAR-10 data.
-cifar10_dir = 'pku-deep-learning/lg-course/assignment1/data/cifar-10-batches-py' 
+
+cifar10_dir = '../data/cifar-10-batches-py'
 X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 # As a sanity check, we print out the size of the training and test data.
 print('Training data shape: ', X_train.shape)     # (50000,32,32,3)
 print('Training labels shape: ', y_train.shape)   # (50000L,)
 print('Test data shape: ', X_test.shape)        # (10000,32,32,3)
 print('Test labels shape: ', y_test.shape)      # (10000L,)
-print()
+
+
+import matplotlib.pyplot as plt
+# Visualize some examples from the dataset.
+# We show a few examples of training images from each class.
+classes = ['plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+num_classes = len(classes)
+samples_per_class = 7
+for y, cls in enumerate(classes):
+    idxs = np.flatnonzero(y_train == y)
+    idxs = np.random.choice(idxs, samples_per_class, replace=False)
+    for i, idx in enumerate(idxs):
+        plt_idx = i * num_classes + y + 1
+        plt.subplot(samples_per_class, num_classes, plt_idx)
+        plt.imshow(X_train[idx].astype('uint8'))
+        plt.axis('off')
+        if i == 0:
+            plt.title(cls)
+plt.show()
+
+
 
 
 
