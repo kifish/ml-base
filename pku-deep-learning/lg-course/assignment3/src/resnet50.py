9262e3239b83d526874bb8d6a62a26a73763fc41
kifish <huakaihuakai@qq.com>
2019-04-23 11:32:22 +0800

    buf

--- pku-deep-learning/lg-course/assignment3/src/resnet50.py
+++ pku-deep-learning/lg-course/assignment3/src/resnet50.py
@@ -6,7 +6,7 @@ from keras import backend as K
 import tensorflow as tf
 import numpy as np
 import pickle
-base_model = resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=Input(shape=(64,64,3)), pooling=None, classes=1000)
+base_model = resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=Input(shape=(64,64,3)), pooling=True, classes=1000)
 def cal_seq_acc(out,labels):
     labels = np.asarray(labels).squeeze()
     num_feature,num_sample = labels.shape
@@ -20,6 +20,7 @@ def cal_seq_acc(out,labels):
     seq_acc = np.count_nonzero(np.all(outYarr[:,0:4]==labels[0:4,:].T,axis=1))/ np.float(num_sample) * 100
     return preds, Y_out, seq_acc
 
+# input_layer = Input(shape=(None, None, 3))
 x = Flatten()(base_model.output)
 x = Dense(128,activation='relu')(x)
 x = Dropout(0.2)(x)
@@ -31,25 +32,47 @@ pred4 = Dense(11,activation='softmax')(x)
 pred5 = Dense(11,activation='softmax')(x)
 
 # X = tf.placeholder(tf.float32, [None, 64, 64, 3])
-Y = tf.placeholder(tf.int32, [None, 5, 11])
-loss1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 0, :], logits=pred1))
-loss2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 1, :], logits=pred2))
-loss3 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 2, :], logits=pred3))
-loss4 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 3, :], logits=pred4))
-loss5 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 4, :], logits=pred5))
-loss = loss1 + loss2 + loss3 +loss4 + loss5
+# Y = tf.placeholder(tf.int32, [None, 5, 11])
+# loss1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 0, :], logits=pred1))
+# loss2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 1, :], logits=pred2))
+# loss3 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 2, :], logits=pred3))
+# loss4 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 3, :], logits=pred4))
+# loss5 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y[:, 4, :], logits=pred5))
+# loss = loss1 + loss2 + loss3 +loss4 + loss5
 
 # multi_accuracy =
-model = Model(input=base_model.input,output = [pred1,pred2,pred3,pred4,pred5])
-model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+outputs =  [pred1,pred2,pred3,pred4,pred5]
+model = Model(input=base_model.input,output = outputs)
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
 with open('../data/train_rgb.pkl', 'rb') as f:
     X_train, Y_train = pickle.load(f)
 
-with open('../data/test.pkl_rgb', 'rb') as f:
+with open('../data/test_rgb.pkl', 'rb') as f:
     X_test, Y_test = pickle.load(f)
 
+n_train = X_train.shape[0]
+Y_train = [
+    Y_train[:, 0, :].reshape(n_train,11),
+    Y_train[:, 1, :].reshape(n_train,11),
+    Y_train[:, 2, :].reshape(n_train,11),
+    Y_train[:, 3, :].reshape(n_train,11),
+    Y_train[:, 4, :].reshape(n_train,11)
+]
+
+n_test = X_test.shape[0]
+Y_test = [
+    Y_test[:, 0, :].reshape(n_test,11),
+    Y_test[:, 1, :].reshape(n_test,11),
+    Y_test[:, 2, :].reshape(n_test,11),
+    Y_test[:, 3, :].reshape(n_test,11),
+    Y_test[:, 4, :].reshape(n_test,11)
+]
+
+#
+# print(Y_train.shape)
+# print(Y_test.shape)
 history = model.fit(x = X_train,y = Y_train,
                                  batch_size = 128,
                                  epochs= 10,
