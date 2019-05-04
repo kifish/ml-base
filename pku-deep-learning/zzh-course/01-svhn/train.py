a3e10f5562dd7a43b85cf48551ca9a247356f18c
kifish <huakaihuakai@qq.com>
2019-05-04 11:40:29 +0800

    q2

--- pku-deep-learning/zzh-course/01-svhn/train.py
+++ pku-deep-learning/zzh-course/01-svhn/train.py
@@ -67,9 +67,7 @@ def main():
                             tf.cast(tf.argmax(label_onehot, 1), dtype=tf.int32))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
     loss_reg = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-    eps = 1e-6
-    cliped_preds = tf.clip_by_value(preds,eps,1-eps)
-    loss = tf.reduce_mean(-tf.reduce_sum(tf.cast(label_onehot,dtype=tf.float32)*tf.log(cliped_preds),axis=1)) + loss_reg
+    loss = tf.losses.mean_squared_error(label_onehot, logits) + loss_reg
 
     ## train config
     global_steps = tf.Variable(0, trainable=False)
