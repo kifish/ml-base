8300812151458e320a64154a5fb2b5a826e40177
kifish <huakaihuakai@qq.com>
2019-05-06 14:02:21 +0800

    fix

--- pku-deep-learning/lg-course/assignment3/src/utils.py
+++ pku-deep-learning/lg-course/assignment3/src/utils.py
@@ -317,7 +317,7 @@ def load_detected_data(root_path,results_img_path,verbose = False):
             digits = []
             for info in infos:
                 digits.append(info[0])
-            X.append(np.array(img))
+            X.append(np.array(img,dtype=np.float32))
         Y.append(digits)
     X = np.array(X)
     X = normalize(X)
