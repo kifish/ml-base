72d795d91a680af69cf9b290f014eeac3a25d141
kifish <huakaihuakai@qq.com>
2019-05-08 14:50:21 +0800

    fix q3

--- pku-deep-learning/zzh-course/01-svhn/model.py
+++ pku-deep-learning/zzh-course/01-svhn/model.py
@@ -21,6 +21,18 @@ class Model():
         self.bias_init = tf.zeros_initializer()
         self.reg = tf_contrib.layers.l2_regularizer(config.weight_decay)
 
+    def gaussian_kernel_init(self,edge_len,mean=0.,std=1.0):
+        d = tf.distributions.Normal(mean, std)
+        if edge_len % 2 == 1:
+            vals = d.prob(tf.range(start=-(edge_len // 2), limit=(edge_len // 2) + 1, delta=1, dtype=tf.float32))
+        else:
+            vals = d.prob(tf.range(start=-(edge_len - 1), limit=(edge_len - 1) + 1, delta=2, dtype=tf.float32))
+        gauss_kernel = tf.einsum('i,j->ij',
+                                 vals,
+                                 vals)
+        gauss_kernel = gauss_kernel / tf.reduce_sum(gauss_kernel)
+        return gauss_kernel[:, :, tf.newaxis, tf.newaxis]
+
     def _conv_layer(self, name, inp, kernel_shape, stride, padding='SAME',is_training=False):
         with tf.variable_scope(name) as scope:
             conv_filter = tf.get_variable(name='filter', shape=kernel_shape,
@@ -44,11 +56,10 @@ class Model():
                                padding=padding, name=name, data_format='NHWC')
         else:
             with tf.variable_scope(name) as scope:
-                conv_filter = tf.get_variable(name='filter', shape=shape,
-                                initializer=tf.random_normal_initializer(), regularizer=self.reg,trainable=False)
+                gauss_kernel = tf.get_variable(name='gauss_kernel',initializer=self.gaussian_kernel_init(shape[0]),trainable=False)
                 p = mode
                 x = tf.pow(inp,p)
-                x = tf.nn.conv2d(x,conv_filter,strides=[1,stride,stride,1],padding=padding,name = name,data_format='NHWC')
+                x = tf.nn.conv2d(x,gauss_kernel,strides=[1,stride,stride,1],padding=padding,name = name,data_format='NHWC')
                 x = tf.pow(x,1/p)
         return x
 
@@ -82,7 +93,7 @@ class Model():
         x = self._conv_layer(name='conv1', inp=data,
                              kernel_shape=[3, 3, config.nr_channel, 16], stride=1,
                              is_training=is_training) # Nx32x32x16
-        x = self._pool_layer(name='pool1', inp=x, ksize=None, padding='SAME',shape=[3,3,16,16],stride=2, mode=2) # Nx16x16x16
+        x = self._pool_layer(name='pool1', inp=x, ksize=None, padding='SAME',shape= [3,3,16,16],stride=2, mode=2) # Nx16x16x16
 
         # conv2
         x = self._conv_layer(name='conv21', inp=x, kernel_shape=[3, 3, 16, 32],
@@ -96,14 +107,14 @@ class Model():
                              stride=1, is_training=is_training)
         x = self._conv_layer(name='conv32', inp=x, kernel_shape=[3, 3, 64, 64],
                              stride=1, is_training=is_training) # Nx8x8x64
-        x = self._pool_layer(name='pool3', inp=x, ksize=None, padding = 'SAME',shape=[5,5,64,64],stride=2, mode=2) # Nx4x4x64
+        x = self._pool_layer(name='pool3', inp=x, ksize=None, padding = 'SAME',shape= [5,5,64,64],stride=2, mode=2) # Nx4x4x64
 
         # conv4
         x = self._conv_layer(name='conv41', inp=x, kernel_shape=[3, 3, 64, 128],
                              stride=1, is_training=is_training)
         x = self._conv_layer(name='conv42', inp=x, kernel_shape=[3, 3, 128, 128],
                              stride=1, is_training=is_training) # Nx4x4x128
-        x = self._pool_layer(name='pool4', inp=x, ksize=None, padding='VALID', shape=[4, 4, 128, 128], stride=1, mode=2) # Nx1x1x128
+        x = self._pool_layer(name='pool4', inp=x, ksize=None, padding='VALID', shape= [4, 4, 128, 128], stride=1, mode=2) # Nx1x1x128
 
         # fc1
         logits = self._fc_layer(name='fc1', inp=x, units=config.nr_class, dropout=0)
