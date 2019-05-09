06894e557066d342373d73d8bf194c88992dae6e
kifish <huakaihuakai@qq.com>
2019-05-09 12:32:52 +0800

    nmt

--- pku-deep-learning/lg-course/assignment4/src/third_library/translate.py
+++ pku-deep-learning/lg-course/assignment4/src/third_library/translate.py
@@ -1,14 +1,20 @@
 import os, sys
 from dataloader import *
 from keras.models import load_model
-
+from transformer import Transformer
+from keras.optimizers import *
 dim_model = 256
 
 itokens, otokens = MakeS2SDict('../../dataset/train_chn2eng_w.txt', dict_file='../../dataset/chn2eng_w_word.txt')
 X_test, Y_test = MakeS2SData('../../dataset/test_chn2eng_w.txt', itokens, otokens,
                              h5_file='../../dataset/chn2eng_w.test.h5')
+
+dim_model = 256
+s2s = Transformer(itokens, otokens, len_limit=70, dim_model=dim_model, d_inner_hid=512, \
+				   n_head=8, layers=2, dropout=0.1)
+s2s.compile(Adam(0.0001, 0.9, 0.98, epsilon=1e-9))
 save_path = '../../tmp/chn2eng.model.h5'
-s2s = load_model(save_path)
+s2s.model.load_weights(save_path)
 
 if 'eval' in sys.argv:
     print(s2s.decode_sequence_readout('A black dog eats food .'.split(), delimiter=' '))
