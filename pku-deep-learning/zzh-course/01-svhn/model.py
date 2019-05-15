a6ec2bb4d64c0ffb4589e06c793614fdd3277f8e
kifish <huakaihuakai@qq.com>
2019-05-15 15:07:58 +0800

    recover 01-svhn

--- pku-deep-learning/zzh-course/01-svhn/model.py
+++ pku-deep-learning/zzh-course/01-svhn/model.py
@@ -17,25 +17,11 @@ class Model():
     def __init__(self):
         # set the initializer of conv_weight and conv_bias
         self.weight_init = tf_contrib.layers.variance_scaling_initializer(factor=1.0,
-                                mode='FAN_IN', uniform=False)
+                                                                          mode='FAN_IN', uniform=False)
         self.bias_init = tf.zeros_initializer()
         self.reg = tf_contrib.layers.l2_regularizer(config.weight_decay)
 
-    def gaussian_kernel_init(self,shape, mean=0., std=1.0):
-        edge_len = shape[0]
-        d = tf.distributions.Normal(mean, std)
-        if edge_len % 2 == 1:
-            vals = d.prob(tf.range(start=-(edge_len // 2), limit=(edge_len // 2) + 1, delta=1, dtype=tf.float32))
-        else:
-            vals = d.prob(tf.range(start=-(edge_len - 1), limit=(edge_len - 1) + 1, delta=2, dtype=tf.float32))
-        gauss_kernel = tf.einsum('i,j->ij', vals, vals)
-        gauss_kernel = gauss_kernel / tf.reduce_sum(gauss_kernel)
-        gauss_kernel = tf.stack([gauss_kernel] * shape[2])
-        gauss_kernel = tf.stack([gauss_kernel] * shape[3])
-        gauss_kernel = tf.transpose(gauss_kernel, perm=[3, 2, 0, 1])
-        return gauss_kernel
-
-    def _conv_layer(self, name, inp, kernel_shape, stride, padding='SAME',is_training=False):
+    def _conv_layer(self, name, inp, kernel_shape, stride, padding='SAME', is_training=False):
         with tf.variable_scope(name) as scope:
             conv_filter = tf.get_variable(name='filter', shape=kernel_shape,
                                           initializer=self.weight_init, regularizer=self.reg)
@@ -48,31 +34,14 @@ class Model():
             x = tf.nn.relu(x)
         return x
 
-    def _pool_layer(self, name, inp, ksize, stride, shape = None,padding='SAME', mode='MAX',version = 'naive'):
-        assert (mode in ['MAX', 'AVG'] or isinstance(mode,int)), 'the mode of pool must be MAX or AVG or Interger'
+    def _pool_layer(self, name, inp, ksize, stride, padding='SAME', mode='MAX'):
+        assert mode in ['MAX', 'AVG'], 'the mode of pool must be MAX or AVG'
         if mode == 'MAX':
             x = tf.nn.max_pool(inp, ksize=[1, ksize, ksize, 1], strides=[1, stride, stride, 1],
                                padding=padding, name=name, data_format='NHWC')
         elif mode == 'AVG':
             x = tf.nn.avg_pool(inp, ksize=[1, ksize, ksize, 1], strides=[1, stride, stride, 1],
                                padding=padding, name=name, data_format='NHWC')
-        else:
-            p = mode
-            if version != 'naive': #按照论文实现
-                if p < 100:
-                    with tf.variable_scope(name) as scope:
-                        gauss_kernel = tf.get_variable(name='gauss_kernel',initializer=self.gaussian_kernel_init(shape),trainable=False)
-                        x = tf.clip_by_value(tf.pow(inp,p),tf.float32.min + 1.0,tf.float32.max-1.0)
-                        x = tf.nn.conv2d(x,gauss_kernel,strides=[1,stride,stride,1],padding=padding,name = name,data_format='NHWC')
-                        x = tf.clip_by_value(tf.pow(x,1/p),tf.float32.min + 1.0,tf.float32.max-1.0)
-                else:
-                    with tf.variable_scope(name) as scope:
-                        x = tf.nn.max_pool(inp,ksize=[1,shape[0],shape[0],1],strides=[1,stride,stride,1],padding=padding,name = name,data_format='NHWC')
-            else: #按照pytorch里的lp pooling实现
-                x = tf.pow(inp,p)
-                x = tf.nn.avg_pool(x, ksize=[1, shape[0], shape[0], 1], strides=[1, stride, stride, 1],
-                                   padding=padding, name=name, data_format='NHWC')
-                x = tf.pow(x,1/p)
         return x
 
     def _fc_layer(self, name, inp, units, dropout=0.5):
@@ -81,7 +50,7 @@ class Model():
             dim = 1
             for d in shape[1:]:
                 dim *= d
-            x = tf.reshape(inp, [-1, dim]) # flatten
+            x = tf.reshape(inp, [-1, dim])  # flatten
             if dropout > 0:
                 x = tf.nn.dropout(x, keep_prob=dropout, name='dropout')
             x = tf.layers.dense(x, units, kernel_initializer=self.weight_init,
@@ -104,32 +73,37 @@ class Model():
         # conv1
         x = self._conv_layer(name='conv1', inp=data,
                              kernel_shape=[3, 3, config.nr_channel, 16], stride=1,
-                             is_training=is_training) # Nx32x32x16
-        x = self._pool_layer(name='pool1', inp=x, ksize=None, padding='SAME',shape= [3,3,16,16],stride=2, mode=4) # Nx16x16x16
+                             is_training=is_training)  # Nx32x32x32
+        x = self._pool_layer(name='pool1', inp=x, ksize=2,
+                             stride=2, mode='MAX')  # Nx16x16x16
 
         # conv2
         x = self._conv_layer(name='conv21', inp=x, kernel_shape=[3, 3, 16, 32],
                              stride=1, is_training=is_training)
         x = self._conv_layer(name='conv22', inp=x, kernel_shape=[3, 3, 32, 32],
-                             stride=1, is_training=is_training) # Nx16x16x32
-        x = self._pool_layer(name='pool2', inp=x, ksize=None, padding='SAME',shape= [4,4,32,32],stride=2, mode=4) # Nx8x8x32
+                             stride=1, is_training=is_training)
+        x = self._pool_layer(name='pool2', inp=x, ksize=2,
+                             stride=2, mode='MAX')  # Nx8x8x32
 
         # conv3
         x = self._conv_layer(name='conv31', inp=x, kernel_shape=[3, 3, 32, 64],
                              stride=1, is_training=is_training)
         x = self._conv_layer(name='conv32', inp=x, kernel_shape=[3, 3, 64, 64],
-                             stride=1, is_training=is_training) # Nx8x8x64
-        x = self._pool_layer(name='pool3', inp=x, ksize=None, padding = 'SAME',shape= [5,5,64,64],stride=2, mode=4) # Nx4x4x64
+                             stride=1, is_training=is_training)
+        x = self._pool_layer(name='pool3', inp=x, ksize=2,
+                             stride=2, mode='MAX')  # Nx4x4x64
 
         # conv4
         x = self._conv_layer(name='conv41', inp=x, kernel_shape=[3, 3, 64, 128],
                              stride=1, is_training=is_training)
         x = self._conv_layer(name='conv42', inp=x, kernel_shape=[3, 3, 128, 128],
-                             stride=1, is_training=is_training) # Nx4x4x128
-        x = self._pool_layer(name='pool4', inp=x, ksize=None, padding='VALID', shape= [4, 4, 128, 128], stride=1, mode=4) # Nx1x1x128
+                             stride=1, is_training=is_training)
+        x = self._pool_layer(name='pool4', inp=x, ksize=4,
+                             stride=4, mode='AVG')  # Nx1x1x128
 
         # fc1
-        logits = self._fc_layer(name='fc1', inp=x, units=config.nr_class, dropout=0)
+        logits = self._fc_layer(
+            name='fc1', inp=x, units=config.nr_class, dropout=0)
 
         placeholders = {
             'data': data,
