a6719a4bf000f7b408beb32e2e29648cf1f1745b
kifish <huakaihuakai@qq.com>
2019-05-16 19:30:40 +0800

    buf

--- pku-deep-learning/wxj-course/语义计算与知识检索/assignment3/src/third_library/lang2logic-PyTorch/seq2seq/geoqueries/attention/main.py
+++ pku-deep-learning/wxj-course/语义计算与知识检索/assignment3/src/third_library/lang2logic-PyTorch/seq2seq/geoqueries/attention/main.py
@@ -254,7 +254,7 @@ if __name__ == "__main__":
     main_arg_parser.add_argument('-num_layers', type=int, default=1, help='number of layers in the LSTM')
     main_arg_parser.add_argument('-dropout',type=float, default=0.5,help='dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
     main_arg_parser.add_argument('-dropoutrec',type=int,default=0,help='dropout for regularization, used after each c_i. 0 = no dropout')
-    main_arg_parser.add_argument('-enc_seq_length',type=int, default=40,help='number of timesteps to unroll for')
+    main_arg_parser.add_argument('-enc_seq_length',type=int, default=50,help='number of timesteps to unroll for') #改大一点
     main_arg_parser.add_argument('-dec_seq_length',type=int, default=100,help='number of timesteps to unroll for')
     main_arg_parser.add_argument('-batch_size',type=int, default=20,help='number of sequences to train on in parallel')
     main_arg_parser.add_argument('-max_epochs',type=int, default=90,help='number of full passes through the training data')
