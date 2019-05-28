aaeb7e38f3905c0a593c2fdfc2d3d9bd6b140480
kifish <huakaihuakai@qq.com>
2019-05-28 21:34:52 +0800

    q2

--- pku-deep-learning/zzh-course/05-texture-network/train.py
+++ pku-deep-learning/zzh-course/05-texture-network/train.py
@@ -50,7 +50,7 @@ def main():
     noise = tf.Variable(tf.nn.sigmoid(pre_noise))
 
     '''load texture image, notice that the pixel value has to be normalized to [0,1]'''
-    image = cv2.imread('./images/red-peppers256.jpg')
+    image = cv2.imread('./images/robot.jpg')
     image = cv2.resize(image, image_shape[1:3])
     image = image.reshape(image_shape)
     image = (image/255).astype('float32')
