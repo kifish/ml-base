3f7a1ffdaf41d980b69f806fe1d43aadacc7fa95
kifish <huakaihuakai@qq.com>
2019-05-29 16:18:23 +0800

    q4

--- pku-deep-learning/zzh-course/05-texture-network/train.py
+++ pku-deep-learning/zzh-course/05-texture-network/train.py
@@ -12,7 +12,6 @@ import numpy as np
 '''use 13 convolution layers to generate gram matrix'''
 
 GRAM_LAYERS= ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2', 'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3', 'conv5_1', 'conv5_2', 'conv5_3']
-SELECTED_LAYERS = ['pool1','pool2', 'pool3','pool4']
 image_shape = (1, 224, 224, 3)
 
 '''you need to complete this method'''
@@ -22,18 +21,15 @@ def get_l2_gram_loss_for_layer(noise, source, layer):
     shape = source_feature_map.get_shape().as_list()
     n_channel = shape[-1]
     n_pixel = shape[1] * shape[2]
-    F_target = tf.reshape(source_feature_map,[-1,n_channel]) # n_pixel * n_channel  
-    G_target = tf.matmul(tf.transpose(F_target),F_target) #  n_channel * n_piexl * n_pixel * n_channel
-    F = tf.reshape(noise_feature_map, [-1, n_channel])
-    G = tf.matmul(tf.transpose(F),F)
-    loss = tf.reduce_sum(tf.pow(G_target - G,2)) / (4 * n_channel * n_channel * n_pixel * n_pixel)
+    F_target = tf.reshape(source_feature_map,[-1,n_pixel]) # n_channel * n_pixel
+    F = tf.reshape(noise_feature_map, [-1, n_pixel])
+    loss = tf.reduce_sum(tf.pow(tf.nn.top_k(F_target, n_pixel).values - tf.nn.top_k(F, n_pixel).values, 2))
     loss = loss * 255 * 255 * 13
     return loss 
 
 def get_gram_loss(noise, source):
     with tf.name_scope('get_gram_loss'):
-        gram_loss = [get_l2_gram_loss_for_layer(
-            noise, source, layer) for layer in SELECTED_LAYERS]
+        gram_loss = [get_l2_gram_loss_for_layer(noise, source, layer) for layer in GRAM_LAYERS]
     return tf.reduce_mean(tf.convert_to_tensor(gram_loss))
 
 def output_img(session, x, save=False, out_path=None):
