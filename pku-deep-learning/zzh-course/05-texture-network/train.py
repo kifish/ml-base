6f0a11da7ce4931eb79c0d69185fe5b216e629ce
kifish <huakaihuakai@qq.com>
2020-07-26 21:53:41 +0800

    update

--- pku-deep-learning/zzh-course/05-texture-network/train.py
+++ pku-deep-learning/zzh-course/05-texture-network/train.py
@@ -11,49 +11,44 @@ import numpy as np
 
 '''use 13 convolution layers to generate gram matrix'''
 
-GRAM_LAYERS = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2',
-               'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3', 'conv5_1', 'conv5_2', 'conv5_3']
+GRAM_LAYERS = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2', 'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3', 'conv5_1', 'conv5_2', 'conv5_3']
+SELECTED_LAYERS = ['conv1_1', 'conv1_2', 'pool1', 'conv2_1', 'conv2_2', 'pool2', 'conv3_1', 'conv3_2', 'conv3_3','pool3']
 image_shape = (1, 224, 224, 3)
 
 '''you need to complete this method'''
 def get_l2_gram_loss_for_layer(noise, source, layer):
-    source_feature_map = getattr(source, layer)
-    noise_feature_map = getattr(noise, layer)
+    source_feature_map = getattr(source,layer)
+    noise_feature_map = getattr(noise,layer)
     shape = source_feature_map.get_shape().as_list()
     n_channel = shape[-1]
     n_pixel = shape[1] * shape[2]
-    source_feature_map = tf.transpose(source_feature_map,perm=[0,3,1,2])
-    F_target = tf.reshape(source_feature_map, [-1, n_pixel]) # n_channel * n_pixel
-    noise_feature_map = tf.transpose(noise_feature_map,perm=[0,3,1,2])
-    F = tf.reshape(noise_feature_map, [-1, n_pixel])
-    loss = tf.reduce_sum(tf.pow(tf.nn.top_k(
-        F_target, n_pixel).values - tf.nn.top_k(F, n_pixel).values, 2))
-    loss = loss * 255 * 13
-    return loss
-
+    F_target = tf.reshape(source_feature_map,[-1,n_channel]) # n_pixel * n_channel  
+    G_target = tf.matmul(tf.transpose(F_target),F_target) #  n_channel * n_piexl * n_pixel * n_channel
+    F = tf.reshape(noise_feature_map, [-1, n_channel])
+    G = tf.matmul(tf.transpose(F),F)
+    loss = tf.reduce_sum(tf.pow(G_target - G,2)) / (4 * n_channel * n_channel * n_pixel * n_pixel)
+    loss = loss * 255 * 255 * 13
+    return loss 
 
 def get_gram_loss(noise, source):
     with tf.name_scope('get_gram_loss'):
-        gram_loss = [get_l2_gram_loss_for_layer(
-            noise, source, layer) for layer in GRAM_LAYERS]
+        gram_loss = [get_l2_gram_loss_for_layer(noise, source, layer) for layer in SELECTED_LAYERS ]
     return tf.reduce_mean(tf.convert_to_tensor(gram_loss))
-
 
 def output_img(session, x, save=False, out_path=None):
     shape = image_shape
-    img = np.clip(session.run(x), 0, 1) * 255
+    img = np.clip(session.run(x),0, 1) * 255
     img = img.astype('uint8')
     if save:
         cv2.imwrite(out_path, (np.reshape(img, shape[1:])))
 
-
 def main():
     '''training a image initialized with noise'''
-    pre_noise = tf.Variable(tf.random_uniform(image_shape, -3, 3))
+    pre_noise = tf.Variable(tf.random_uniform(image_shape, -3, 3 ))
     noise = tf.Variable(tf.nn.sigmoid(pre_noise))
 
     '''load texture image, notice that the pixel value has to be normalized to [0,1]'''
-    image = cv2.imread('./images/red-peppers256.jpg')
+    image = cv2.imread('../../images/red-peppers256.jpg')
     image = cv2.resize(image, image_shape[1:3])
     image = image.reshape(image_shape)
     image = (image/255).astype('float32')
@@ -73,7 +68,7 @@ def main():
 
     total_loss = loss
 
-    global_steps = tf.Variable(0, trainable=False)
+    global_steps = tf.Variable(0, trainable = False)
     values = [0.01, 0.005, 0.001]
     lr = tf.train.piecewise_constant(global_steps, [200, 1500], values)
 
@@ -88,37 +83,40 @@ def main():
                                          tf.get_default_graph())
 
     ''' create a session '''
-    tf.set_random_seed(12345)  # ensure consistent results
+    tf.set_random_seed(12345) # ensure consistent results
     global_cnt = 0
     epoch_start = 0
     g_list = tf.global_variables()
     saver = tf.train.Saver(var_list=g_list)
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())  # init all variables
+        sess.run(tf.global_variables_initializer()) # init all variables
 
         ## training
         for epoch in range(epoch_start+1, config.nr_epoch+1):
             global_cnt += 1
-            _, loss, summary = sess.run([update_image, total_loss, merged],
-                                        feed_dict={global_steps: global_cnt})
+            _, loss, summary =  sess.run([update_image, total_loss, merged],
+                                        feed_dict ={ global_steps: global_cnt} )
 
             if global_cnt % config.show_interval == 0:
                 train_writer.add_summary(summary, global_cnt)
                 print(
-                    "e:{}".format(epoch), 'loss: {:.5f}'.format(loss),
+                    "e:{}".format(epoch),'loss: {:.5f}'.format(loss),
                 )
 
             '''save the trained image every 10 epoch'''
-            if global_cnt % config.save_interval == 0 and global_cnt > 0:
-                out_dir = os.path.dirname(
-                    os.path.realpath(__file__)) + '/./output'
+            if global_cnt % config.save_interval == 0 and global_cnt >0 :
+                out_dir = os.path.dirname(os.path.realpath(__file__)) + '/./output'
 
                 if not os.path.isdir(out_dir):
                     os.makedirs(out_dir)
-                out_dir = out_dir + '/{}.png'.format(global_cnt)
-                output_img(sess, noise, save=True, out_path=out_dir)
+                out_dir = out_dir +'/{}.png'.format(global_cnt)
+                output_img(sess, noise, save=True, out_path = out_dir)
+
+
 
         print('Training is done, exit.')
+
+
 
 
 if __name__ == "__main__":
